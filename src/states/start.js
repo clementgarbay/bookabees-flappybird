6342fd02db8b509695c5c9cd6fd6530f21b05e2e
Clément Garbay <clementgarbay@gmail.com>
2017-08-25 14:06:50 +0100

    Update start state

--- src/states/start.js
+++ src/states/start.js
@@ -1,3 +1,4 @@
+import Phaser from 'phaser'
 import { restartGame } from '../main'
 
 class Start {
@@ -6,6 +7,7 @@ class Start {
   }
 
   preload () {
+    this.game.load.image('title', 'assets/images/title.png')
     this.game.load.image('btnStart', 'assets/images/btn_start.png')
     this.game.load.image('background', 'assets/images/bg.png')
   }
@@ -21,6 +23,18 @@ class Start {
     overlay.drawRect(5, 5, this.game.world.width - 10, this.game.world.height - 5)
     overlay.endFill()
 
+    // Title
+    const titleImg = this.game.cache.getImage('title')
+    this.game.add.sprite(this.game.world.centerX - titleImg.width * 0.25, 80, 'title').scale.setTo(0.5, 0.5)
+
+    // Instruction
+    this.game.add
+      .text(0, 0, 'You need to press SPACE to make the bird fly.', { font: '15px Arial', fill: '#fff', boundsAlignH: 'center' })
+      .setTextBounds(0, 160, this.game.world.width, 20)
+    this.game.add
+      .text(0, 0, 'Beware of the clouds!', { font: '15px Arial', fill: '#fff', boundsAlignH: 'center' })
+      .setTextBounds(0, 180, this.game.world.width, 20)
+
     // Button try again
     const btnStartImg = this.game.cache.getImage('btnStart')
     this.game.add.button(
@@ -29,6 +43,9 @@ class Start {
       'btnStart',
       restartGame
     ).scale.setTo(0.5, 0.5)
+
+    // Map spacebar to start game function
+    this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(restartGame)
   }
 }
 
